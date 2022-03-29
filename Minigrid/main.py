46c3f659e883a9fa0e627a0390941f62ea26f106
Joel Furtak <57066915+JoelFurtak@users.noreply.github.com>
2022-03-29 22:17:28 +0200

    Update main.py

--- Minigrid/main.py
+++ Minigrid/main.py
@@ -28,28 +28,44 @@ from datetime import datetime
 #parser.add_argument('--epsilon', type= float, default=1.0, help='Exploration rate.')
 #args = parser.parse_args()
 
-env_list = ['MiniGrid-Empty-6x6-v0', 'MiniGrid-Empty-Random-6x6-v0', 'MiniGrid-FourRooms-v0']
-env_short_name_list = ['mpt6x6', 'rnd6x6', '4rooms']
-env_index = 1
+env_list = ['MiniGrid-Empty-6x6-v0', 'MiniGrid-Empty-Random-6x6-v0', 'MiniGrid-DoorKey-6x6-v0']
+env_short_name_list = ['mpt6x6', 'rnd6x6', 'dk6x6']
+env_index = 2
 env_name = env_list[env_index]
 env_short_name = env_short_name_list[env_index]
 
-mem_size = 20000
-seed = 123
+mem_size = 100000
+seed = 12
 epsilon = 1.0
 epsilon_min = 0.05
-update_epsilon = 500
+#update_epsilon = 500
+update_epsilon = 5
 epsilon_decay = 0.95
 batch_size= 1028
 target_update = 10
+episodes = 1000
 
 gamma = 0.99
 learning_rate = 0.0001
-hidden_shape = 35
+hidden_shape = 256
 tau = 0.005
 
 dqn_agent = DQN_Agent(env_name, seed, mem_size, gamma, epsilon, epsilon_min, update_epsilon, epsilon_decay, batch_size, target_update, hidden_shape, learning_rate, tau)
-dqn_agent.train(500)
+dqn_agent.train(500, env_short_name, 1)
+
+
+
+# Data Collector
+#for index in range(len(env_short_name_list)):
+#    print("Current Environment: {}".format(env_short_name_list[index]))
+#    env_name = env_list[index]
+#    env_short_name = env_short_name_list[index]
+    #dqn_agent = DQN_Agent(env_name, seed, mem_size, gamma, epsilon, epsilon_min, update_epsilon, epsilon_decay, batch_size, target_update, hidden_shape, learning_rate, tau)
+#    for run in range(5):
+#        print("Run: {}".format(run + 1))
+#        dqn_agent = DQN_Agent(env_name, seed, mem_size, gamma, epsilon, epsilon_min, update_epsilon, epsilon_decay, batch_size, target_update, hidden_shape, learning_rate, tau)
+#        dqn_agent.train(episodes, env_short_name, run)
+
 
 # Training Loop
 '''
