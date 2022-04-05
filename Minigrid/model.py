8f57b8013b126561ee48c8a3d378ffb629ed9359
Joel Furtak <57066915+JoelFurtak@users.noreply.github.com>
2022-04-05 12:53:56 +0200

    added actor and critic for ppo

--- Minigrid/model.py
+++ Minigrid/model.py
@@ -1,11 +1,16 @@
+from turtle import forward, st
+import numpy as np
+import math
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 from torch.nn.utils import spectral_norm
+from torch.nn import init
+from torch.distributions.categorical import Categorical
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class DQN(nn.Module):
     def __init__(self, state_size, action_size, hidden_shape):
@@ -13,7 +18,7 @@ class DQN(nn.Module):
         self.Q = nn.Sequential(
             nn.Linear(state_size, hidden_shape),
             nn.ReLU(),
-            spectral_norm(nn.Linear(hidden_shape, hidden_shape)),
+            nn.Linear(hidden_shape, hidden_shape),
             nn.ReLU(),
             nn.Linear(hidden_shape, action_size),
             nn.Identity()
@@ -23,3 +28,49 @@ class DQN(nn.Module):
         q = self.Q(x)
         return q
 
+
+class PPOActorNetwork(nn.Module):
+    def __init__(self, n_actions, input_dims, alpha,
+            fc1_dims=256, fc2_dims=256):
+        super(PPOActorNetwork, self).__init__()
+
+        self.actor = nn.Sequential(
+                nn.Linear(*input_dims, fc1_dims),
+                nn.ReLU(),
+                nn.Linear(fc1_dims, fc2_dims),
+                nn.ReLU(),
+                nn.Linear(fc2_dims, n_actions),
+                nn.Softmax(dim=-1)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = device
+        self.to(self.device)
+
+    def forward(self, state):
+        dist = self.actor(state)
+        dist = Categorical(dist)
+        
+        return dist
+
+    
+class PPOCriticNetwork(nn.Module):
+    def __init__(self, input_dims, alpha, fc1_dims=256, fc2_dims=256):
+        super(PPOCriticNetwork, self).__init__()
+
+        self.critic = nn.Sequential(
+                nn.Linear(*input_dims, fc1_dims),
+                nn.ReLU(),
+                nn.Linear(fc1_dims, fc2_dims),
+                nn.ReLU(),
+                nn.Linear(fc2_dims, 1)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = device
+        self.to(self.device)
+
+    def forward(self, state):
+        value = self.critic(state)
+
+        return value
