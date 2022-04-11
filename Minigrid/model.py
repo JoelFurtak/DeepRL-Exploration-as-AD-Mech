8e8d20acdd2f6d027812d27bf3fec689becbb468
Joel Furtak <57066915+JoelFurtak@users.noreply.github.com>
2022-04-11 17:59:38 +0200

    added spectral norm dqn

--- Minigrid/model.py
+++ Minigrid/model.py
@@ -28,6 +28,22 @@ class DQN(nn.Module):
         q = self.Q(x)
         return q
 
+class SpectralDQN(nn.Module):
+    def __init__(self, state_size, action_size, hidden_shape):
+        super(DQN, self).__init__()
+        self.Q = nn.Sequential(
+            nn.Linear(state_size, hidden_shape),
+            nn.ReLU(),
+            spectral_norm(nn.Linear(hidden_shape, hidden_shape)),
+            nn.ReLU(),
+            nn.Linear(hidden_shape, action_size),
+            nn.Identity()
+        )
+
+    def forward(self, x):
+        q = self.Q(x)
+        return q
+
 
 class PPOActorNetwork(nn.Module):
     def __init__(self, n_actions, input_dims, alpha,
