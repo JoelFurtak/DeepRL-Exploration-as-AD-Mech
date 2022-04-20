5cad0294ae18ae8a4a4c9dbbc3319ec4e99d054f
Joel Furtak <57066915+JoelFurtak@users.noreply.github.com>
2022-04-20 11:53:49 +0200

    added RND Models

--- Minigrid/model.py
+++ Minigrid/model.py
@@ -1,4 +1,4 @@
-from turtle import forward, st
+from turtle import forward, hideturtle, st
 import numpy as np
 import math
 import torch
@@ -90,3 +90,43 @@ class PPOCriticNetwork(nn.Module):
         value = self.critic(state)
 
         return value
+
+class RNDPredictor(nn.Module):
+    def __init__(self, input_dims, outpit_dims, learning_rate, hidden_dims=256):
+        super(RNDPredictor, self).__init__()
+
+        self.predictor = nn.Sequential(
+            nn.Linear(1, hidden_dims),
+            nn.ReLU(),
+            nn.Linear(hidden_dims, hidden_dims),
+            nn.ReLU(),
+            nn.Linear(hidden_dims, hidden_dims)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        self.to(device)
+
+    def forward(self, state):
+        prediction = self.predictor(state)
+
+        return prediction
+
+class RNDTarget(nn.Module):
+    def __init__(self, input_dims, output_dims, learning_rate, hidden_dims=256):
+        super(RNDTarget, self).__init__()
+
+        self.target = nn.Sequential(
+            nn.Linear(1, hidden_dims),
+            nn.ReLU(),
+            nn.Linear(hidden_dims, hidden_dims),
+            nn.ReLU(),
+            nn.Linear(hidden_dims, hidden_dims)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        self.to(device)
+
+    def forward(self, state):
+        target = self.target(state)
+
+        return target
