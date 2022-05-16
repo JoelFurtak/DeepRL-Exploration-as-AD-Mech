167f9c31a183d31d1ebb63c02898da9aae0b77c2
Joel Furtak <57066915+JoelFurtak@users.noreply.github.com>
2022-05-16 23:21:49 +0200

    save model

--- Minigrid/rnd.py
+++ Minigrid/rnd.py
@@ -118,11 +118,12 @@ class RNDAgent:
         self.memory.clear_memory()
 
     def save(self, alg, env_name, run):
-        torch.save(self.actor.state_dict(), f"./model/{alg}_{env_name}_{run}.pth")
-        torch.save(self.critic.state_dict(), f"./model/{alg}_{env_name}_{run}.pth")
-        torch.save(self.predictor.state_dict(), f"./model/{alg}_{env_name}_{run}.pth")
+        print('... saving model ...')
+        torch.save(self.actor.state_dict(), f"./model/{alg}_{env_name}_{run}_actor.pth")
+        torch.save(self.critic.state_dict(), f"./model/{alg}_{env_name}_{run}_critic.pth")
+        torch.save(self.predictor.state_dict(), f"./model/{alg}_{env_name}_{run}_predictor.pth")
 
     def load(self, alg, env_name, run):
-        self.actor.load_state_dict(torch.load(f"./model/{alg}_{env_name}_{run}.pth"))        
-        self.critic.load_state_dict(torch.load(f"./model/{alg}_{env_name}_{run}.pth"))
-        self.predictor.load_state_dict(torch.load(f"./model/{alg}_{env_name}_{run}.pth"))
+        self.actor.load_state_dict(torch.load(f"./model/{alg}_{env_name}_{run}_actor.pth"))        
+        self.critic.load_state_dict(torch.load(f"./model/{alg}_{env_name}_{run}_critic.pth"))
+        self.predictor.load_state_dict(torch.load(f"./model/{alg}_{env_name}_{run}_predictor.pth"))
