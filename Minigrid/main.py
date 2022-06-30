967cce8da8c1ad728bf491b23d8372ffe6b9acdf
Joel Furtak <57066915+JoelFurtak@users.noreply.github.com>
2022-06-30 11:12:48 +0200

    multiple runs

--- Minigrid/main.py
+++ Minigrid/main.py
@@ -12,7 +12,7 @@ from gym_minigrid.wrappers import FlatObsWrapper, RGBImgObsWrapper, ImgObsWrappe
 from dqn import DQN_Agent, Memory, device
 from ppo import PPOAgent
 from rnd import RNDAgent
-from utils import save_data, plot_average
+from utils import save_data, plot_average, Minigrid2Image
 
 import argparse
 
@@ -32,9 +32,9 @@ import argparse
 #parser.add_argument('--epsilon', type= float, default=1.0, help='Exploration rate.')
 #args = parser.parse_args()
 
-env_list = ['MiniGrid-Empty-6x6-v0', 'MiniGrid-Empty-Random-6x6-v0', 'MiniGrid-DoorKey-6x6-v0', 'MiniGrid-MultiRoom-N4-S5-v0', 'MiniGrid-KeyCorridorS3R3-v0']
+env_list = ['MiniGrid-Empty-6x6-v0', 'MiniGrid-Empty-Random-6x6-v0', 'MiniGrid-DoorKey-6x6-v0', 'MiniGrid-MultiRoom-N4-S5-v0', 'MiniGrid-KeyCorridorS3R2-v0']
 env_short_name_list = ['mpt6x6', 'rnd6x6', 'dk6x6', 'multi', 'keyco']
-env_index = 3
+env_index = 4
 env_name = env_list[env_index]
 env_short_name = env_short_name_list[env_index]
 seed = 1005
@@ -167,11 +167,11 @@ save_data(episodes=current_episode, scores=score_history, collisions=collision_c
 env = gym.make(env_name)
 env = FlatObsWrapper(env)
 env.seed(seed)
-N = 256                                   # 4096 8192                       Multi 256 RND
-batch_size = 64                           # 128 512 1024 2048
+N = 1024                                   # 4096 8192                       Multi 256 RND
+batch_size = 256                           # 128 512 1024 2048
 n_epochs = 4
 alpha = 0.0003
-alg = 'rnd'
+alg = 'ppo'
 train = True
 play = False
 
@@ -179,7 +179,7 @@ ppo_agent = PPOAgent(n_actions=env.action_space.n, batch_size=batch_size, alpha=
 rnd_agent = RNDAgent(n_actions=env.action_space.n, batch_size=batch_size, lr=alpha, n_epochs=n_epochs, input_dims=env.observation_space.shape)
 
 novelid = False
-rnd = True
+rnd = False
 if rnd:
     agent = rnd_agent
 else:
@@ -209,88 +209,89 @@ curriculum = False
 if curriculum:
     agent.load(alg=alg, env_name=env_short_name, run=1)
 
-if train:       
-    for i in range(episodes):
-        obs = env.reset()
-        done = False
-        score = 0
-        current_episode.append(i+1)
-        collisions = 0
-        pick_up = 0
-        drop = 0
-        toggle = 0
-        key_pickup = 0
-        key_drop = 0
-        door_toggle = 0
-        turns = 0
-        ep_intrinsic_reward = 0
-        intrinsic_reward = 0
-        states = []
-        while not done:
-            #env.render()
-            states.append(obs)
-            action, prob, val = agent.choose_action(obs)
-            obs_, reward, done, info = env.step(action)
-            if rnd:
-                intrinsic_reward = agent.intrinsic_reward(obs)
-                if (np.std(total_rewards) > 0):
-                    intrinsic_reward /= np.std(total_rewards)
-                else:
-                    intrinsic_reward = intrinsic_reward
-                total_int_reward.append(intrinsic_reward)
-                total_rewards = np.array(total_int_reward)
-                intrinsic_reward *= intrinsic_reward_coef                                            #0.00003
-                if novelid:
-                    for state in states:
-                        if np.array_equal(obs_, state):
-                            intrinsic_reward *= 0
-                reward += intrinsic_reward
-            n_steps += 1
-            score += reward - intrinsic_reward
-            ep_intrinsic_reward += intrinsic_reward
-            agent.remember(obs, action, prob, val, reward, done)
-            if ((action == 0) or (action == 1)):
-                turns += 1
-            if ((action == 2) and (np.array_equal(obs, obs_))):
-                collisions += 1
-            if (action == 3):
-                pick_up += 1
-            if ((action == 3) and (not np.array_equal(obs, obs_))):
-                key_pickup += 1
-            if (action == 4):
-                drop += 1
-            if ((action == 4) and (not np.array_equal(obs, obs_))):
-                key_drop += 1
-            if (action == 5):
-                toggle += 1
-            if ((action == 5) and (not np.array_equal(obs, obs_))):
-                door_toggle += 1
-            
-            if n_steps % N == 0:
-                agent.learn()
-                learn_iters += 1
-            obs = obs_
-        collision_counter.append(collisions)
-        pick_up_counter.append(pick_up)
-        drops_counter.append(drop)
-        toggles_counter.append(toggle)
-        turn_counter.append(turns)
-        key_pickups.append(key_pickup)
-        key_drops.append(key_drop)
-        doors_toggled.append(door_toggle)
-        score_history.append(score)
-        avg_score = np.mean(score_history[-100:])
-        ep_total_int_reward.append(ep_intrinsic_reward)
+for j in range(1, 6):
+    if train:       
+        for i in range(episodes):
+            obs = env.reset()
+            done = False
+            score = 0
+            current_episode.append(i+1)
+            collisions = 0
+            pick_up = 0
+            drop = 0
+            toggle = 0
+            key_pickup = 0
+            key_drop = 0
+            door_toggle = 0
+            turns = 0
+            ep_intrinsic_reward = 0
+            intrinsic_reward = 0
+            states = []
+            while not done:
+                #env.render()
+                states.append(obs)
+                action, prob, val = agent.choose_action(obs)
+                obs_, reward, done, info = env.step(action)
+                if rnd:
+                    intrinsic_reward = agent.intrinsic_reward(obs)
+                    if (np.std(total_rewards) > 0):
+                        intrinsic_reward /= np.std(total_rewards)
+                    else:
+                        intrinsic_reward = intrinsic_reward
+                    total_int_reward.append(intrinsic_reward)
+                    total_rewards = np.array(total_int_reward)
+                    intrinsic_reward *= intrinsic_reward_coef                                            #0.00003
+                    if novelid:
+                        for state in states:
+                            if np.array_equal(obs_, state):
+                                intrinsic_reward *= 0
+                    reward += intrinsic_reward
+                n_steps += 1
+                score += reward - intrinsic_reward
+                ep_intrinsic_reward += intrinsic_reward
+                agent.remember(obs, action, prob, val, reward, done)
+                if ((action == 0) or (action == 1)):
+                    turns += 1
+                if ((action == 2) and (np.array_equal(obs, obs_))):
+                    collisions += 1
+                if (action == 3):
+                    pick_up += 1
+                if ((action == 3) and (not np.array_equal(obs, obs_))):
+                    key_pickup += 1
+                if (action == 4):
+                    drop += 1
+                if ((action == 4) and (not np.array_equal(obs, obs_))):
+                    key_drop += 1
+                if (action == 5):
+                    toggle += 1
+                if ((action == 5) and (not np.array_equal(obs, obs_))):
+                    door_toggle += 1
+                
+                if n_steps % N == 0:
+                    agent.learn()
+                    learn_iters += 1
+                obs = obs_
+            collision_counter.append(collisions)
+            pick_up_counter.append(pick_up)
+            drops_counter.append(drop)
+            toggles_counter.append(toggle)
+            turn_counter.append(turns)
+            key_pickups.append(key_pickup)
+            key_drops.append(key_drop)
+            doors_toggled.append(door_toggle)
+            score_history.append(score)
+            avg_score = np.mean(score_history[-100:])
+            ep_total_int_reward.append(ep_intrinsic_reward)
 
-        if avg_score > best_score:
-            best_score = avg_score
+            if avg_score > best_score:
+                best_score = avg_score
 
-        print(f'Episode: {i+1}, Score: {score:.2f}, Intrinsic Reward: {ep_intrinsic_reward:.2f}, Avg Score: {avg_score:.2f}, Steps done: {n_steps}, Learning Steps done: {learn_iters}') #, \nCollisions: {collisions}, Pick ups: {pick_up}, Drops: {drop}, Toggles: {toggle}, Keys picked up: {key_pickup}, Keys dropped: {key_drop}, Doors toggled: {door_toggle}, Turns: {turns}')
+            print(f'Episode: {i+1}, Score: {score:.2f}, Intrinsic Reward: {ep_intrinsic_reward:.2f}, Avg Score: {avg_score:.2f}, Steps done: {n_steps}, Learning Steps done: {learn_iters}, Run: {j}') #, \nCollisions: {collisions}, Pick ups: {pick_up}, Drops: {drop}, Toggles: {toggle}, Keys picked up: {key_pickup}, Keys dropped: {key_drop}, Doors toggled: {door_toggle}, Turns: {turns}')
 
-    agent.save(alg=alg, env_name=env_short_name, run=1)
-    save_data(episodes=current_episode, scores=score_history, collisions=collision_counter, pick_ups=pick_up_counter, drops=drops_counter, \
-        toggles=toggles_counter, key_pickups=key_pickups, key_drops=key_drops, door_toggles=doors_toggled, turns=turn_counter, intrinsic_reward=ep_total_int_reward,\
-        alg=alg, short_name=env_short_name, run=1, train=train)
+        agent.save(alg=alg, env_name=env_short_name, run=j)
+        save_data(episodes=current_episode, scores=score_history, collisions=collision_counter, pick_ups=pick_up_counter, drops=drops_counter, \
+            toggles=toggles_counter, key_pickups=key_pickups, key_drops=key_drops, door_toggles=doors_toggled, turns=turn_counter, intrinsic_reward=ep_total_int_reward,\
+            alg=alg, short_name=env_short_name, run=j, train=train)
 
 if play:
     agent.load(alg=alg, env_name=env_short_name, run=1)
@@ -374,4 +375,4 @@ if play:
 
     save_data(episodes=current_episode, scores=score_history, collisions=collision_counter, pick_ups=pick_up_counter, drops=drops_counter, \
         toggles=toggles_counter, key_pickups=key_pickups, key_drops=key_drops, door_toggles=doors_toggled, turns=turn_counter, intrinsic_reward=ep_total_int_reward,\
-        alg=alg, short_name=env_short_name, run=1, train=train)
+        alg=alg, short_name=env_short_name, run=5, train=train)
