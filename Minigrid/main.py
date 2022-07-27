c19cca7b380e32322287efd8aafd20c880213753
Joel Furtak <57066915+JoelFurtak@users.noreply.github.com>
2022-07-27 23:13:54 +0200

    Update main.py

--- Minigrid/main.py
+++ Minigrid/main.py
@@ -32,135 +32,13 @@ import argparse
 #parser.add_argument('--epsilon', type= float, default=1.0, help='Exploration rate.')
 #args = parser.parse_args()
 
-env_list = ['MiniGrid-Empty-6x6-v0', 'MiniGrid-Empty-Random-6x6-v0', 'MiniGrid-DoorKey-6x6-v0', 'MiniGrid-MultiRoom-N4-S5-v0', 'MiniGrid-KeyCorridorS3R2-v0']
-env_short_name_list = ['mpt6x6', 'rnd6x6', 'dk6x6', 'multi', 'keyco']
-env_index = 2
+env_list = ['MiniGrid-Empty-6x6-v0', 'MiniGrid-Empty-Random-6x6-v0', 'MiniGrid-DoorKey-16x16-v0', 'MiniGrid-MultiRoom-N4-S5-v0', 'MiniGrid-KeyCorridorS3R2-v0', 'MiniGrid-LavaCrossingS9N2-v0']
+env_short_name_list = ['mpt6x6', 'rnd6x6', 'dk16x16', 'multi', 'keyco', 'lava']
+env_index = 5
 env_name = env_list[env_index]
 env_short_name = env_short_name_list[env_index]
 seed = 1005
-#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#DQN agent
-'''
-env = FlatObsWrapper(gym.make(env_name))
-env.seed(seed)
 
-state_size = env.observation_space.shape[0]
-action_size = env.action_space.n
-
-print(f'states_size: {state_size}')
-
-mem_size = 100000
-epsilon = 1.0
-epsilon_min = 0.05
-update_epsilon = 5
-epsilon_decay = 0.97
-batch_size= 2056
-target_update = 10
-
-gamma = 0.99
-learning_rate = 0.0001
-hidden_shape = 256
-tau = 0.005
-
-dqn_agent = DQN_Agent(state_size, action_size, mem_size, gamma, epsilon, batch_size, target_update, hidden_shape, learning_rate, tau)
-
-episodes = 500
-score_history = []
-current_episode = []
-learn_iters = 0
-avg_score = 0
-n_steps = 0
-collision_counter = []
-pick_up_counter = []
-drops_counter = []
-toggles_counter = []
-turn_counter = []
-key_pickups = []
-key_drops = []
-doors_toggled = []
-total_rewards = np.zeros(episodes)
-total_int_reward = []
-ep_total_int_reward = []
-
-#if rnd:
-#    agent = rnd_agent
-#    total_int_reward = []
-#    ep_total_int_reward = []
-
-for i in range(episodes):
-    state = env.reset()
-    steps = 0
-    current_episode.append(i + 1)
-    collisions = 0
-    pick_up = 0
-    drop = 0
-    toggle = 0
-    key_pickup = 0
-    key_drop = 0
-    door_toggle = 0
-    turns = 0
-    int_reward = 0
-    for t in count():
-        steps += 1
-        #env.render()
-        action = dqn_agent.choose_action(state)
-        next_state, reward, done, _ = env.step(action)
-
-        if done and steps != env.max_steps:
-            done_win = True
-        else:
-            done_win = False
-        
-        dqn_agent.remember(state, action, next_state, reward, done_win)
-
-        if ((action == 0) or (action == 1)):
-            turns += 1
-        if ((action == 2) and (np.array_equal(state, next_state))):
-            collisions += 1
-        if (action == 3):
-            pick_up += 1
-        if ((action == 3) and (not np.array_equal(state, next_state))):
-            key_pickup += 1
-        if (action == 4):
-            drop += 1
-        if ((action == 4) and (not np.array_equal(state, next_state))):
-            key_drop += 1
-        if (action == 5):
-            toggle += 1
-        if ((action == 5) and (not np.array_equal(state, next_state))):
-            door_toggle += 1
-    
-        n_steps += 1
-        state = next_state
-        dqn_agent.learn()
-        if done:
-            score_history.append(reward)
-            collision_counter.append(collisions)
-            pick_up_counter.append(pick_up)
-            drops_counter.append(drop)
-            toggles_counter.append(toggle)
-            turn_counter.append(turns)
-            key_pickups.append(key_pickup)
-            key_drops.append(key_drop)
-            doors_toggled.append(door_toggle)
-            ep_total_int_reward.append(int_reward)
-            break
-    avg_score = np.mean(score_history[-100:])
-        
-    #print('Episode: ', i + 1, ' Score: %.1f' % reward, ' Avg Score: %.1f' % avg_score, ' Steps done: ', dqn_agent.steps_done, ' Exploration: %.1f' % dqn_agent.epsilon)
-    print(f'Episode: {i+1}, Score: {reward:.2f}, Avg. Score: {avg_score:.2f}, Steps Done: {n_steps}, Epsilon: {dqn_agent.epsilon:.2f}')
-
-    if i % target_update == 0:
-        dqn_agent.target_net.load_state_dict(dqn_agent.policy_net.state_dict())
-    if current_episode[-1] % update_epsilon == 0:                        # steps_done changed to episodes done
-            dqn_agent.epsilon *= epsilon_decay
-            dqn_agent.epsilon = max(epsilon_min, dqn_agent.epsilon) 
-
-env.close()
-print("Training Complete!")
-save_data(episodes=current_episode, scores=score_history, collisions=collision_counter, pick_ups=pick_up_counter, drops=drops_counter, toggles=toggles_counter, key_pickups=key_pickups, key_drops=key_drops, door_toggles=doors_toggled, turns=turn_counter, intrinsic_reward=ep_total_int_reward,\
-    alg='dqn', short_name=env_short_name, run=2)
-'''
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #PPO/ RND agent Training
 
@@ -231,11 +109,14 @@ if train:
         doors_toggled = []
         total_int_reward = []
         ep_total_int_reward = []
+        death_counter = []
+        total_steps = []
         total_rewards = np.zeros(episodes)
         agent = PPOAgent(n_actions=env.action_space.n, batch_size=batch_size, alpha=alpha, n_epochs=n_epochs, input_dims=env.observation_space.shape)       
         for i in range(episodes):
             obs = env.reset()
             done = False
+            ep_steps = 0
             score = 0
             current_episode.append(i+1)
             collisions = 0
@@ -249,6 +130,7 @@ if train:
             ep_intrinsic_reward = 0
             intrinsic_reward = 0
             states = []
+            death = False
             while not done:
                 #env.render()
                 states.append(obs)
@@ -269,6 +151,7 @@ if train:
                                 intrinsic_reward *= 0
                     reward += intrinsic_reward
                 n_steps += 1
+                ep_steps += 1
                 score += reward - intrinsic_reward
                 ep_intrinsic_reward += intrinsic_reward
                 agent.remember(obs, action, prob, val, reward, done)
@@ -288,7 +171,9 @@ if train:
                     toggle += 1
                 if ((action == 5) and (not np.array_equal(obs, obs_))):
                     door_toggle += 1
-                
+                if(done and reward == 0 and ep_steps < env.max_steps):
+                    death = True
+
                 if n_steps % N == 0:
                     agent.learn()
                     learn_iters += 1
@@ -304,15 +189,17 @@ if train:
             score_history.append(score)
             avg_score = np.mean(score_history[-100:])
             ep_total_int_reward.append(ep_intrinsic_reward)
+            death_counter.append(death)
+            total_steps.append(n_steps)
 
             if avg_score > best_score:
                 best_score = avg_score
 
-            print(f'Episode: {i+1}, Score: {score:.2f}, Intrinsic Reward: {ep_intrinsic_reward:.2f}, Avg Score: {avg_score:.2f}, Steps done: {n_steps}, Learning Steps done: {learn_iters}, Run: {j}') #, \nCollisions: {collisions}, Pick ups: {pick_up}, Drops: {drop}, Toggles: {toggle}, Keys picked up: {key_pickup}, Keys dropped: {key_drop}, Doors toggled: {door_toggle}, Turns: {turns}')
+            print(f'Episode: {i+1}, Score: {score:.2f}, Intrinsic Reward: {ep_intrinsic_reward:.2f}, Avg Score: {avg_score:.2f}, Steps done: {n_steps}, Agent Died: {death}, Learning Steps done: {learn_iters}, Run: {j}') #, \nCollisions: {collisions}, Pick ups: {pick_up}, Drops: {drop}, Toggles: {toggle}, Keys picked up: {key_pickup}, Keys dropped: {key_drop}, Doors toggled: {door_toggle}, Turns: {turns}')
 
         agent.save(alg=alg, env_name=env_short_name, run=j)
-        save_data(episodes=current_episode, scores=score_history, collisions=collision_counter, pick_ups=pick_up_counter, drops=drops_counter, \
-            toggles=toggles_counter, key_pickups=key_pickups, key_drops=key_drops, door_toggles=doors_toggled, turns=turn_counter, intrinsic_reward=ep_total_int_reward,\
+        save_data(episodes=current_episode, steps=total_steps, scores=score_history, collisions=collision_counter, pick_ups=pick_up_counter, drops=drops_counter, \
+            toggles=toggles_counter, key_pickups=key_pickups, key_drops=key_drops, door_toggles=doors_toggled, turns=turn_counter, intrinsic_reward=ep_total_int_reward, death=death_counter,\
             alg=alg, short_name=env_short_name, run=j, train=train)
 
 if play:
@@ -333,9 +220,11 @@ if play:
         doors_toggled = []
         total_int_reward = []
         ep_total_int_reward = []
+        death_counter = []
+        total_steps = []
         total_rewards = np.zeros(episodes)
         agent = PPOAgent(n_actions=env.action_space.n, batch_size=batch_size, alpha=alpha, n_epochs=n_epochs, input_dims=env.observation_space.shape)
-        agent.load(alg=alg, env_name=env_short_name, run=j)
+        agent.load(alg=alg, env_name='lava', run=j)
         for i in range(episodes):
             obs = env.reset()
             done = False
@@ -352,6 +241,8 @@ if play:
             ep_intrinsic_reward = 0
             intrinsic_reward = 0
             states = []
+            death = False
+            ep_steps = 0
             while not done:
                 #env.render()
                 states.append(obs)
@@ -373,6 +264,7 @@ if play:
                     reward += intrinsic_reward
                     #print(f'debug: int reward: {intrinsic_reward}')
                 n_steps += 1
+                ep_steps += 1
                 score += reward - intrinsic_reward
                 ep_intrinsic_reward += intrinsic_reward
                 #agent.remember(obs, action, prob, val, reward, done)
@@ -392,6 +284,8 @@ if play:
                     toggle += 1
                 if ((action == 5) and (not np.array_equal(obs, obs_))):
                     door_toggle += 1
+                if(done and reward == 0 and ep_steps < env.max_steps):
+                    death = True
                 
                 #if n_steps % N == 0:
                 #    agent.learn()
@@ -408,12 +302,14 @@ if play:
             score_history.append(score)
             avg_score = np.mean(score_history[-100:])
             ep_total_int_reward.append(ep_intrinsic_reward)
+            death_counter.append(death)
+            total_steps.append(n_steps)
 
             if avg_score > best_score:
                 best_score = avg_score
 
-            print(f'Episode: {i+1}, Score: {score:.2f}, Intrinsic Reward: {ep_intrinsic_reward:.2f}, Avg Score: {avg_score:.2f}, Steps done: {n_steps}, Run: {j}')
+            print(f'Episode: {i+1}, Score: {score:.2f}, Intrinsic Reward: {ep_intrinsic_reward:.2f}, Avg Score: {avg_score:.2f}, Steps done: {n_steps}, Agent Died: {death}, Run: {j}')
 
-        save_data(episodes=current_episode, scores=score_history, collisions=collision_counter, pick_ups=pick_up_counter, drops=drops_counter, \
-            toggles=toggles_counter, key_pickups=key_pickups, key_drops=key_drops, door_toggles=doors_toggled, turns=turn_counter, intrinsic_reward=ep_total_int_reward,\
+        save_data(episodes=current_episode, steps=total_steps, scores=score_history, collisions=collision_counter, pick_ups=pick_up_counter, drops=drops_counter, \
+            toggles=toggles_counter, key_pickups=key_pickups, key_drops=key_drops, door_toggles=doors_toggled, turns=turn_counter, intrinsic_reward=ep_total_int_reward, death=death_counter,\
             alg=alg, short_name=env_short_name, run=j, train=train)
